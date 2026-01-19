6712f9a6be508ec94b74be2d429f2ce0193b957b
Mathis FLOCH <mathis.floch2025@campus-eni.fr>
2026-01-19 14:08:46 +0100

    fix pipelines

--- src/main/java/com/devops/cicd/PasswordPolicy.java
+++ src/main/java/com/devops/cicd/PasswordPolicy.java
@@ -15,10 +15,15 @@ public class PasswordPolicy {
         boolean hasSpecial = false;
 
         for (char c : password.toCharArray()) {
-            if (Character.isUpperCase(c)) hasUpper = true;
-            else if (Character.isLowerCase(c)) hasLower = true;
-            else if (Character.isDigit(c)) hasDigit = true;
-            else if (!Character.isLetterOrDigit(c)) hasSpecial = true;
+            if (Character.isUpperCase(c)) {
+                hasUpper = true;
+            } else if (Character.isLowerCase(c)) {
+                hasLower = true;
+            } else if (Character.isDigit(c)) {
+                hasDigit = true;
+            } else if (!Character.isLetterOrDigit(c)) {
+                hasSpecial = true;
+            }
         }
 
         return hasUpper && hasLower && hasDigit && hasSpecial;
